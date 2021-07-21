7fd1fdf7a1dd89a58b9c94689d10b2af27feb710
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-07-21 09:42:54 +0300

    Complete first task

---
+++ day-3-task-1/task-1.js
@@ -0,0 +1,16 @@
+const text = 'Дуже цікавий Дуже?~! текст ...текст Собака пес собака';
+
+const getWordRepetitionMap = (text) => {
+    const dividedText = text.toLowerCase().replace(/[^a-zA-Zа-яА-Яії ]+/g, "").split(' ');
+    let wordsMap = new Map();
+    dividedText.forEach(word => {
+        if (wordsMap.has(word)) {
+            wordsMap.set(word, wordsMap.get(word) + 1);
+        } else {
+            wordsMap.set(word, 1);
+        }
+    })
+    return [...wordsMap]
+}
+
+console.log(getWordRepetitionMap(text));
